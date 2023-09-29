5aa98dc4be5be9124fd5000dcc27fcf8f120a40a
sum117 <75037449+sum117@users.noreply.github.com>
2023-09-29 03:52:01 -0300

    fix:  bug in RP message guard

--- src/lib/guards/index.ts
+++ src/lib/guards/index.ts
@@ -4,9 +4,9 @@ import {credentials} from "../../data/credentials";
 
 export const isValidRoleplayMessage: GuardFunction<ArgsOf<"messageCreate" | "messageReactionAdd">> = async ([messageOrReaction], _client, next) => {
   const channel = messageOrReaction instanceof Message ? messageOrReaction.channel : messageOrReaction.message.channel;
-  const author = messageOrReaction instanceof Message ? messageOrReaction.author : messageOrReaction.message.author;
+  const isBot = messageOrReaction instanceof Message ? messageOrReaction.author?.bot : messageOrReaction.me;
 
-  if (channel.type !== ChannelType.GuildText || author?.bot) return;
+  if (channel.type !== ChannelType.GuildText || isBot) return;
   const isRoleplayingChannel = channel.parent?.name.startsWith("RP") || credentials.channels.randomRoleplay === channel.id;
   if (!isRoleplayingChannel) {
     return;
