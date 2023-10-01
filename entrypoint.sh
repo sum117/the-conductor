977cf2873a72575aca20c7cc19f7151c0fead0c9
sum117 <75037449+sum117@users.noreply.github.com>
2023-10-01 20:39:03 -0300

    refactor: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-if [ -z "$(ls -A /app/prisma)" ]; then
-   echo "The directory /app/prisma is empty. Initializing..."
-   cp -a /app_default/prisma/. /app/prisma/
-   rm -rf /app_default/prisma
-fi
+initialize_directory() {
+    local dir_path="$1"
+    local init_action="$2"
 
-if [ -z "$(ls -A /app/cache)" ]; then
-   echo "The directory /app/cache is empty. Initializing..."
-   echo "This directory is used to cache files for the imageVideoMerge command" > /app/cache/README.txt
-fi
+    if [ -z "$(ls -A "$dir_path")" ]; then
+        echo "The directory $dir_path is empty. Initializing..."
+        $init_action
+    fi
+}
+
+copy_prisma_files() {
+    cp -a /app_default/prisma/. /app/prisma/
+    rm -rf /app_default/prisma
+}
+
+create_cache_readme() {
+    echo "This directory is used to cache files for the imageVideoMerge command" > /app/cache/README.txt
+}
+
+initialize_directory "/app/prisma" copy_prisma_files
+initialize_directory "/app/cache" create_cache_readme
 
 exec bun start
