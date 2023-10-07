d4005223709c6fa61a7820a583bc0abcc97334bb
sum117 <jvcaliman@hotmail.com>
2023-10-07 10:13:54 -0300

    fix: update vite env

--- apps/website/data/constants.ts
+++ apps/website/data/constants.ts
@@ -1,8 +1,8 @@
 export const INFO_BOX_FIELDS = ["gender", "weight", "height", "age", "userId"] as const;
 
-export const DISCORD_OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${import.meta.env.DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(
-  import.meta.env.WEBSITE_BASE_URL + "/login",
-)}&response_type=code&scope=identify`;
+export const DISCORD_OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${
+  import.meta.env.VITE_DISCORD_CLIENT_ID
+}&redirect_uri=${encodeURIComponent(import.meta.env.VITE_WEBSITE_BASE_URL + "/login")}&response_type=code&scope=identify`;
 
 export const SELECT_FIELDS = ["race", "faction"] as const;
 
--- apps/website/routes/login.tsx
+++ apps/website/routes/login.tsx
@@ -3,7 +3,7 @@ import {LoaderFunctionArgs, redirect} from "react-router-dom";
 export async function loader({request}: LoaderFunctionArgs) {
   try {
     const code = new URL(request.url).searchParams.get("code");
-    const response = await fetch(`${import.meta.env.API_BASE_URL}/discord/callback?code=${code}`);
+    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/discord/callback?code=${code}`);
     const data = await response.json();
     localStorage.setItem("user", JSON.stringify(data));
     throw null;
