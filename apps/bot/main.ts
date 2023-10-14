4fa201b2e6c6134aa0c9dbc820742eb8bd988aa6
sum117 <75037449+sum117@users.noreply.github.com>
2023-10-14 03:51:24 -0300

    fix: weird behavior with partials

--- apps/bot/main.ts
+++ apps/bot/main.ts
@@ -21,7 +21,7 @@ export const bot = new Client({
     IntentsBitField.Flags.GuildVoiceStates,
     IntentsBitField.Flags.MessageContent,
   ],
-  partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+  partials: [Partials.Reaction],
   // Debug logs are disabled in silent mode
   silent: false,
 
