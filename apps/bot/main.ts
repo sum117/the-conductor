a691360bfc1fbbe20031676996db4c4a3afc67c6
sum117 <jvcaliman@hotmail.com>
2023-11-08 17:21:20 -0300

    feat: create function to create character from message

--- apps/bot/main.ts
+++ apps/bot/main.ts
@@ -1,5 +1,5 @@
 import {dirname, importx} from "@discordx/importer";
-import {ChannelType, GuildTextBasedChannel, IntentsBitField, Interaction, Message, Partials} from "discord.js";
+import {ChannelType, EmbedBuilder, GuildTextBasedChannel, IntentsBitField, Interaction, Message, Partials} from "discord.js";
 import {Client} from "discordx";
 import cron from "node-cron";
 import ptBr from "translations";
@@ -7,6 +7,8 @@ import {credentials} from "utilities";
 import {prisma} from "./db";
 import {processInstruments, processRoleplayChannel, recursivelyDelete} from "./lib/util/helpers";
 import elysiaServer from "./server";
+import {Prisma} from "@prisma/client";
+import lodash from "lodash";
 
 export const bot = new Client({
   // To use only guild command
@@ -32,7 +34,67 @@ export const bot = new Client({
   },
 });
 
-bot.once("ready", async () => {
+// I've made this function because of a database emergency. This should be removed in the future.
+async function createCharacterFromMessage(message: Message) {
+  type DiscordEmbedField = {name: string; value: string; inline: boolean};
+  const getFieldByName = (fields: Array<DiscordEmbedField>, name: string) => fields?.find((field) => field.name === name)?.value;
+
+  const getUserIdFromContent = (content: string) => {
+    const match = content.match(/<@!?(\d+)>/);
+    return match ? match[1] : null;
+  };
+
+  const hasFields = (fields: unknown): fields is Array<DiscordEmbedField> =>
+    Array.isArray(fields) && fields.every((field) => typeof field === "object" && field && "name" in field && "value" in field) && fields?.length > 0;
+
+  const [allRaces, allInstruments, allFactions] = await Promise.all([prisma.race.findMany(), prisma.instrument.findMany(), prisma.faction.findMany()]);
+  if (!message.embeds.length) throw new Error("No embeds in message");
+
+  const characterSheet = EmbedBuilder.from(message.embeds[0]);
+
+  const fields = characterSheet.data.fields;
+  if (!hasFields(fields)) throw new Error("No fields in embed");
+
+  const [firstName, surname] = characterSheet.data.title?.split(" ") ?? [];
+
+  const raceId = allRaces.find((race) => race.name === getFieldByName(fields, "Raça"))?.id;
+  const sanitizedFactionName = getFieldByName(fields, "Facção")?.split(" ").slice(1).join(" ");
+  console.log(sanitizedFactionName);
+  const factionId = allFactions.find((faction) => faction.name === sanitizedFactionName)?.id;
+
+  const userId = getUserIdFromContent(message.content);
+  if (!userId) throw new Error("User ID not found in message content");
+
+  const user = await prisma.user.findUnique({where: {id: userId}, include: {characters: true}});
+  const hasCharacters = user?.characters?.length && user?.characters?.length > 0;
+  if (hasCharacters) throw new Error("User already has a character");
+
+  const character: Prisma.CharacterCreateInput = {
+    name: firstName,
+    surname,
+    backstory: characterSheet.data.description,
+    age: getFieldByName(fields, "Idade"),
+    personality: getFieldByName(fields, "Personalidade"),
+    race: {connect: {id: raceId}},
+    user: {connectOrCreate: {where: {id: userId}, create: {id: userId}}},
+    appearance: getFieldByName(fields, "Aparência"),
+    height: getFieldByName(fields, "Altura"),
+    weight: getFieldByName(fields, "Peso"),
+    gender: getFieldByName(fields, "Gênero"),
+    faction: factionId ? {connect: {id: factionId}} : undefined,
+    imageUrl: characterSheet.data.image?.url,
+    slug: lodash.kebabCase(characterSheet.data.title ?? ""),
+    isBeingUsed: true,
+    isPending: false,
+    instruments: {create: {quantity: 1, instrument: {connect: {id: lodash.sample(allInstruments)?.id}}}},
+  };
+
+  if (!character.name || !character.surname) throw new Error("Character name or surname not found");
+
+  return prisma.character.create({data: character});
+}
+
+bot.once("ready", async (client) => {
   // Make sure all guilds are cached
   await bot.guilds.fetch();
 
@@ -49,6 +111,20 @@ bot.once("ready", async () => {
   //  await bot.clearApplicationCommands(
   //    ...bot.guilds.cache.map((g) => g.id)
   //  );
+
+  const sheetsChannel = await client.channels.fetch(credentials.channels.approvedChannel);
+  if (!sheetsChannel?.isTextBased()) return;
+
+  const sheets = await sheetsChannel.messages.fetch({limit: 100});
+
+  for (const sheet of sheets.values()) {
+    try {
+      await createCharacterFromMessage(sheet);
+    } catch (error) {
+      console.error("Error creating character from message:", error);
+    }
+  }
+
   console.log("Bot started");
 });
 
